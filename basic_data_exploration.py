1b179316f1a4ff474874fcac8e03fb74ef8bc8ff
thiccboimork <118483938+thiccboimork@users.noreply.github.com>
2023-11-09 21:44:53 -0700

    notesies

--- basic_data_exploration.py
+++ basic_data_exploration.py
@@ -1,5 +1,16 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    predicted_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, predicted_val)
+    return mae
+
 
 # read in csv file name
 dataset = pd.read_csv("csv_name.csv")
@@ -20,3 +31,26 @@ focus_column_X = dataset[dataset_features]
 focus_column_X.describe()
 # print out first few rows
 focus_column_X.head()
+# make a model, specify a random_state number (I've seen most use 42)
+data_model = DecisionTreeRegressor(random_state=1)
+# fit model
+data_model.fit(focus_column_X, focus_column_Y)
+
+print("Making predictions for the following 5 houses: ")
+print(focus_column_X.head())
+print("The predictions are: ")
+# Make the actual predictions from the model! (.head not needed)
+print(data_model.predict(focus_column_X.head()))
+
+predicted_target = data_model.predict(focus_column_X)
+# find the mean absolute error, aka how different we are from the actual data
+mean_absolute_error(focus_column_Y, predicted_target)
+# how to make training and validation data !!
+train_X, val_X, train_y, val_y = train_test_split(focus_column_X, focus_column_Y, random_state=0)
+data_model.fit(train_X, train_y)
+# make predictions from actual model
+data_predictions = data_model.predict(val_X)
+
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    curr_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print(f"Max leaf nodes: {max_leaf_nodes} \t\t Mean Absolute Error: {curr_mae}")
